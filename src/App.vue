1ba8fe7c6a12cc8095f911ad31a7670a838582b5
moustru <xexyz@yandex.ru>
2019-08-01 03:03:05 +0300

    only interface

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,15 @@
                 <div class="main-buy">Покупка: <span class="main-buy-count">{{ cfg.sell }}</span></div>
                 <div class="main-sell">Продажа: <span class="main-sell-count">{{ cfg.buy }}</span></div>
             </div>
+            <div class="main-stats">
+                <span class="main-stats-count">Минимальный курс транзакций: {{ minPrice }}</span>
+                <span class="main-stats-count">Максимальный курс транзакций: {{ maxPrice }}</span>
+            </div>
             <div class="main-info">
                 <div class="main-actions">
                     <h3 class="main-actions-title">Последние операции</h3>
                     <div class="revert">
-                        <action-card v-for="(action, i) in transactions" :key="i" :item="action" :type="currentType"/>
+                        <action-card v-for="(action, i) in history" :key="i" :item="action" :type="cfg.currentType"/>
                     </div>
                 </div>
                 <div class="main-transactions">
@@ -37,81 +41,49 @@
 <script>
     import axios from 'axios';
     import crypto from 'crypto-js';
-    import env from './env';
-    import { serialize } from './bin/utils';
     import ActionCard from './components/ActionCard.vue';
     import Transactions from './components/Transactions.vue';
 
     export default {
         data() {
             return {
-                cfg: {
-                    date: 0,
-                    wallet: 10000,
-                    buy: 0,
-                    sell: 0,
-                    bet: 50,
-                    btc: 0
-                },
-                transactions: [],
-                currentType: 'Buy'
+                cfg: {},
+                order: {},
+                history: [],
+                transactions: []
             }
         },
 
         components: { ActionCard, Transactions },
-
-        methods: {
-            async main() {
-                let res = await axios.get('ticker');
-                this.cfg.buy = Number(res.data.BTC_RUB.buy_price).toFixed(2);
-                this.cfg.sell = Number(res.data.BTC_RUB.sell_price).toFixed(2);
-
-                let purposeForSell = this.transactions.find(x => x.sellPrice <= this.cfg.buy);
-                let purposeForBuy = this.transactions.find(x => x.sellPrice <= this.cfg.sell);
-
-                if(purposeForSell && this.transactions.length > 0) this.sellBTC(purposeForSell);
-                if(!purposeForBuy && this.transactions.length > 0) this.buyBTC();
-
-                return new Promise(resolve => { resolve() })
+        computed: {
+            minPrice() {
+                let array = this.transactions.map(x => x.sellPrice);
+                if(this.allow) return Math.min.apply(null, array);
             },
 
-            buyBTC(e) {
-                if(this.cfg.wallet - this.cfg.bet > 0) {
-                    let transaction = {
-                        BTC: (this.cfg.bet / this.cfg.sell),
-                        amount: Number(this.cfg.bet),
-                        sellPrice: this.cfg.sell,
-                        created: new Date().getTime() / 1000
-                    }
-            
-                    this.cfg.wallet -= this.cfg.bet;
-                    this.transactions.push(transaction);
-                    this.cfg.btc = this.transactions.map(x => x.BTC).reduce((a, b) => a + b);
-                    this.currentType = 'Buy'
-                } else {
-                    e.preventDefault();
-                }
-            },
-
-            sellBTC(transaction) {
-                let amount = transaction.BTC * engine.config.buy;
-
-                this.cfg.wallet += amount;
-                this.cfg.btc -= transaction.BTC;
-                this.currentType = 'Sell';
-                this.transactions.splice(transaction, 1);
-
-                if(this.transactions.length == 0) this.buyBTC();
+            maxPrice() {
+                let array = this.transactions.map(x => x.sellPrice);
+                if(this.allow) return Math.max.apply(null, array);
             }
         },
 
-        mounted() {
-            this.cfg.date = new Date().getTime();
-            this.main().then(() => {  
-                this.buyBTC();
-            })
+        methods: {
+            info() {
+                axios.get('/info').then(res => {
+                    this.cfg = res.data.cfg;
+                    this.order = res.data.order;
+                    this.history = res.data.history;
+                    this.transactions = res.data.transactions;
+                    this.allow = true;
+                })
 
-            setInterval(this.main, 1000);
+
+            },
+        },
+
+        mounted() {
+            this.info();
+            setInterval(this.info, 2000);
         }
     }
 </script>
--- src/bin/interceptors.js
+++ src/bin/interceptors.js
@@ -1,16 +1,8 @@
 import axios from 'axios';
-import env from './../env';
-import { sign, serialize } from './utils';
-
-const postData = serialize({})
 
 axios.interceptors.request.use(settings => {
-    settings.baseURL = 'https://api.exmo.com/v1/';
-    settings.headers = {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8index.html",
-        "Key": env.key,
-        "Sign": sign(postData)
-    }
+    settings.baseURL = 'http://127.0.0.1:8002';
+    settings.headers.Accept = 'application/json'
 
     return settings
 }, err => {
